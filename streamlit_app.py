2d498d6c2d71708a9702968f0e68f28d6a19f536
DaneAnaliza <87229869+mikrutm@users.noreply.github.com>
2025-02-20 10:11:09 +0000

    init

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
-
+import pandas as pd 
+df = pd.read_csv("konsultacje.csv")
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+st.dataframe(df)
